07660a07bfe8637e226c1d6a531a4025c422f11b
AVI-Panwar <avipanwar6565@gmail.com>
2024-08-30 23:14:08 +0530

    first

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// Functional Component For Header 
+const Header = () => {
+  return (
+    <h2>
+        Expense Tracker
+       
+    </h2>
+  )
+}
+
+export default Header
